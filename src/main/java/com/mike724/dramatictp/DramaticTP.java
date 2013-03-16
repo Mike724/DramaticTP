79292c609d9ff2e3d97bf105d7fdaa74fea99929
Michael Meyer <michaelmeyer724@gmail.com>
2013-03-16 13:42:28 -0400

    Base commit

---
+++ src/main/java/com/mike724/dramatictp/DramaticTP.java
@@ -0,0 +1,55 @@
+/*
+    This file is part of DramaticTP.
+
+    DramaticTP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DramaticTP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DramaticTP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.mike724.dramatictp;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
+
+import java.io.IOException;
+import java.lang.Override;
+import java.util.logging.Logger;
+
+public class DramaticTP extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Disabled successfully");
+    }
+
+    @Override
+    public void onEnable() {
+        if (!this.getDataFolder().exists()) {
+            this.getDataFolder().mkdir();
+        }
+
+        FileConfiguration config = this.getConfig();
+        config.options().copyDefaults(true);
+        this.saveConfig();
+
+        Logger log = this.getLogger();
+
+        //Enable plugin metrics
+        try {
+            MetricsLite metrics = new MetricsLite(this);
+            metrics.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.info("Enabled successfully");
+    }
+}
